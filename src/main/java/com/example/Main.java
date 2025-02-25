f4adc22613780c97bd8f81371945eddbc3b4e1b7
harishbitcse82 <harish.bitcse82@gmail.com>
2025-02-25 15:22:02 +0530

    initial commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is my Main Class Hello World!" );
+    }
+}
